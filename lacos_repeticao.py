820874fedfc9ce9ca63255e536eed8cb95910afa
Ubiratan <palomaorlandiw@gmail.com>
2022-06-07 16:20:20 -0300

    lacos_repeticao

---
+++ lacos_repeticao.py
@@ -0,0 +1,14 @@
+# Laços de Repetição + Listas
+
+for palavra in range(1,4):
+  print("Carregando")
+
+for item in range (1,21):
+  print(item)
+
+for item in range(1,21,3):
+  print(item)
+
+nomes = ["jhonatan", "paula", "camila"]
+for nome in nomes:
+  print(nome)
